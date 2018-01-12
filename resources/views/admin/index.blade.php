7993292c629dc55d19bb69f4b39af6543db0b80c
vicleos <510331882@qq.com>
2018-01-12 15:02:47 +0800

    modify some assets uri

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,19 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Dashboard - Vue.js Feed</title>
+    <title>Dashboard</title>
     <link rel="shortcut icon" href="{{ config('website.icon') }}">
     <!-- Fonts -->
     <!-- Bootstrap 3.3.6 -->
     <link rel="stylesheet" href="/lte/css/bootstrap.min.css">
     <!-- Pnotify -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pnotify/3.0.0/pnotify.css">
+    <link rel="stylesheet" href="https://cdn.bootcss.com/pnotify/3.0.0/pnotify.css">
     <link rel="stylesheet" href="/css/pnotify.buttons.css">
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.bootcss.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <!-- Sweetalert -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/sweetalert2/4.0.5/sweetalert2.css">
+    <link rel="stylesheet" href="https://cdn.bootcss.com/limonte-sweetalert2/4.0.5/sweetalert2.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="/lte/css/AdminLTE.css">
     <link rel="stylesheet" href="/lte/css/skin-blue.min.css">
@@ -37,10 +37,10 @@
 {{-- jQuery --}}
 <script src="/lte/js/jQuery-2.2.0.min.js"></script>
 {{-- pnotify --}}
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pnotify/3.0.0/pnotify.js"></script>
+<script src="https://cdn.bootcss.com/pnotify/3.0.0/pnotify.jss"></script>
 <script src="/js/pnotify.buttons.js"></script>
 {{-- Sweetalert --}}
-<script src="https://cdn.jsdelivr.net/sweetalert2/4.0.5/sweetalert2.min.js"></script>
+<script src="https://cdn.bootcss.com/limonte-sweetalert2/4.0.5/sweetalert2.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="/lte/js/bootstrap.min.js"></script>
 {{-- AdminLTE js --}}
