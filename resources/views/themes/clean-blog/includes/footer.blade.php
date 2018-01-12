a3bcedf583efbb9105ae60221b128a032d7ae001
vicleos <510331882@qq.com>
2018-01-12 14:37:53 +0800

    modify some assets uri

--- resources/views/themes/clean-blog/includes/footer.blade.php
+++ resources/views/themes/clean-blog/includes/footer.blade.php
@@ -10,7 +10,7 @@
                         <a href="#">
                                 <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                    <i class="fa fa-weibo fa-stack-1x fa-inverse"></i>
                                 </span>
                         </a>
                     </li>
@@ -18,12 +18,12 @@
                         <a href="#">
                                 <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                    <i class="fa fa-wechat fa-stack-1x fa-inverse"></i>
                                 </span>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="https://github.com/vicleos">
                                 <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-github fa-stack-1x fa-inverse"></i>
